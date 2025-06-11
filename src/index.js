1def57531525f14dd2f989f31d59e16c510d00eb
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2025-06-10 19:28:01 -0600

    Registro de usuarios con POSTMAN

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ const taskRoutes = require('./routes/taskRoute');
 app.use('/api/tasks', taskRoutes);
 
 
+// Rutas para crear usuarios
+const userRoutes = require('./routes/userRoute');
+app.use('/api/users', userRoutes);
+
+
 //Inicializar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
