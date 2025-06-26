64d720eaa790e2f3e5d7b860d2f96ed6b5d6afa6
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2025-06-25 23:07:48 -0600

    Token de usuario al iniciar sesion

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -28,5 +28,5 @@ const login = async (req, res) => {
 }
 
 module.exports = {
-    Login
+    login
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,6 @@ const {login} = require('../controllers/authController');
 
 const router = express.Router();
 //Ruta para iniciar sesion
-route.post('/login', login);
+router.post('/login', login);
 
 module.exports = router;
